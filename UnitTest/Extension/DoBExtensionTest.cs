c4e3f4bd9b31db9ec6d1ddaf8e9f5cd326c7e3d5
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-24 19:04:50 -0300

    add unit test

---
+++ UnitTest/Extension/DoBExtensionTest.cs
@@ -0,0 +1,51 @@
+using CourseSignUp.Extension;
+using System;
+using Xunit;
+
+namespace UnitTest.Extension
+{
+  public class DoBExtensionTest
+  {
+    [Fact]
+    public void GetAge_BirthDay()
+    {
+      // Arrange
+      var age = 30;
+      var date = DateTime.Now.AddYears(-age);
+
+      // Act
+      var result = date.GetAge();
+
+      // Assert
+      Assert.Equal(age, result);
+    }
+
+    [Fact]
+    public void GetAge_BirthDayMinus()
+    {
+      // Arrange
+      var age = 30;
+      var date = DateTime.Now.AddYears(-age).AddDays(-1);
+
+      // Act
+      var result = date.GetAge();
+
+      // Assert
+      Assert.Equal(age, result);
+    }
+
+    [Fact]
+    public void GetAge_BirthDayPlus()
+    {
+      // Arrange
+      var age = 30;
+      var date = DateTime.Now.AddYears(-age).AddDays(+1);
+
+      // Act
+      var result = date.GetAge();
+
+      // Assert
+      Assert.Equal(age-1, result);
+    }
+  }
+}

aead054fd44732d2453eafff39cb0d0d1cf69ad7
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-24 19:05:29 -0300

    add age calculation

---
+++ CourseSignUp/Extension/DoBExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CourseSignUp.Extension
+{
+  public static class DoBExtension
+  {
+    public static int GetAge(this DateTime dob)
+    {
+      int age = DateTime.Now.Year - dob.Year;
+
+      if (dob.Date > DateTime.Now.AddYears(-age))
+        age--;
+
+      return age;
+    }
+  }
+}

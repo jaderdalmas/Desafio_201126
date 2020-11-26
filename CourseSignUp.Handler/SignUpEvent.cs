4779ba9eaa439464021e6f189de54c39ebbbf9c1
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-26 11:00:03 -0300

    add message handler

---
+++ CourseSignUp.Handler/SignUpEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseSignUp.Handler
+{
+  public class SignUpEvent
+  {
+    public string CourseId { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+  }
+}
